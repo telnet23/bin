2a4c158b0c0b50105ef9628080dc510c0187c30d
telnet23 <telnet23@users.noreply.github.com>
2020-08-04 17:42:21 -0400

    reload nginx in cf-ips.sh

--- cf-ips.sh
+++ cf-ips.sh
@@ -7,3 +7,4 @@ curl --location --silent https://www.cloudflare.com/ips-v6 >> cf-ips.new
 cat cf-ips cf-ips.new | grep -P '/\d+' | sort | uniq > cf-ips.tmp
 cat cf-ips.tmp > cf-ips
 cat cf-ips | sed 's/^/set_real_ip_from /' | sed 's/$/;/' > cf-ips.conf
+systemctl reload nginx
